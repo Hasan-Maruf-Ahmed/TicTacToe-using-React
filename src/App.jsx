b2971df2be2392585dafdc27fd7e07fb4ed0bc46
Hasan Maruf Ahmed <hasanmarufahmedfahim@gmail.com>
2023-09-19 04:16:40 +0600

    Made a Board using buttons to play on.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import "./App.css";
 
-function App() {
+function Board() {
   return (
-  <>
-    
-  </>
+    <>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+    </>
   );
 }
 
-export default App;
+export default Board;
