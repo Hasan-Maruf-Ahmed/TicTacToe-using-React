81d407a136b108db77f1f4aed6f7d2ec28d92e31
Hasan Maruf Ahmed <hasanmarufahmedfahim@gmail.com>
2023-09-20 04:56:48 +0600

    Made interactive component to change state when clicked.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,37 @@
+import { useState } from "react";
 import "./App.css";
 
 function Board() {
   return (
     <>
       <div className="board-row">
-        <button className="square">X</button>
-        <button className="square">X</button>
-        <button className="square">X</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square">X</button>
-        <button className="square">X</button>
-        <button className="square">X</button>
+      <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square">X</button>
-        <button className="square">X</button>
-        <button className="square">X</button>
+      <Square />
+        <Square />
+        <Square />
       </div>
+    </>
+  );
+}
+
+function Square() {
+  const [value, setvalue] = useState(null);
+
+  function handleClick(){
+    setvalue('X');
+  }
+  return (
+    <>
+      <button className="square" onClick={handleClick}>{value}</button>
     </>
   );
 }
